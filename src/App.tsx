e7cb11f1c40981ac253684df8f966c6dc8d4949a
Sylwia Milczarek <s.milczarek@aotm.gov.pl>
2025-11-05 10:30:50 +0100

    chore: base config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="m-5 flex">
+        <p className="mx-auto text-xl">This is where it begins...</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
